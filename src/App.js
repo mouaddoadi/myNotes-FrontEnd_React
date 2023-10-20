0a9eb0a66fc0990fdd91d18a2335d99a8524558d
Mouad Doadi <md.doadi00@gmail.com>
2023-10-20 11:34:51 +0100

    [Back-End] GET note

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Header />
         <Routes>
           <Route path='/' element={<NotesListPage />} />
-          <Route path='note/:id' element={<NotePage />} />
+          <Route path='/note/:id' element={<NotePage />} />
         </Routes>
       </div>
     </Router>
--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 const NotePage = () => {
+  const { id } = useParams();
+  let [note, setNotes] = useState(null)
+
+  useEffect( () => {
+    getNote()
+  }, [id])
+
+  let getNote = async () => {
+    let response = await fetch(`/api/notes/${id}`)
+    let data = await response.json()
+    setNotes(data)
+  }
+
   return (
-    <h1>SINGLE NOTE</h1>
-  )
+    <div>
+        <p>
+            { note?.body }
+        </p>
+    </div>
+  );
 }
 
-export default NotePage
+export default NotePage;
