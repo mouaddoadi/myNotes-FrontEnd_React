eae94ec11ba907f5bf7ddf3656a42bbc0be8e82b
Mouad Doadi <md.doadi00@gmail.com>
2023-10-20 11:45:44 +0100

    Link between notes and note

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ListItem = ( { note } ) => {
   return (
-    <h3>
+    <Link to={`/note/${note.id}`}>
+      <h3>
         { note.body }
-    </h3>
+      </h3>
+    </Link>
   )
 }
 
